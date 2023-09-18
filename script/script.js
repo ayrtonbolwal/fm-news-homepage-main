39602d8c98b6823aded81f98f188ca128c4c9a28
Ayrton Raúl Bolaños <ayrtonbolwal@gmail.com>
2023-09-18 10:06:09 -0600

    Commit #4 : Develop : Fixed Mobile-Menu bug

--- script/script.js
+++ script/script.js
@@ -1,14 +1,14 @@
 let burgerButton = document.getElementById("burgerButton");
 let closeButton = document.getElementById("closeButton");
-let mobileMenu = document.getElementById("mobileMenu");
 
 const deployMenu = () => {
-    mobileMenu.style.display = "flex";
-}
+  let mobileMenu = document.getElementById("mobileMenu");
+  mobileMenu.style.display = "flex";
+};
 
 const closeMenu = () => {
-    mobileMenu.style.display = "none";
-}
+  mobileMenu.style.display = "none";
+};
 
 burgerButton.addEventListener("click", deployMenu);
 closeButton.addEventListener("click", closeMenu);
